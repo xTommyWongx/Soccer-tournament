b0b7260a8d86715003951604d112e1998d457004
Bal Kumar Rai <preitam_rai@yahoo.com>
2018-02-09 21:24:04 +0800

    add background

--- ViewRouter.js
+++ ViewRouter.js
@@ -5,6 +5,12 @@ module.exports = class ViewRouter{
     router(){
         const router = express.Router();
         router.get('/',(req,res)=>res.render("home"));
+        router.get('/profile',(req,res)=>res.render("profile"));
+        router.get('/dashboard',(req,res)=>res.render("dashboard"));
+        router.get('/teams',(req,res)=>res.render("teams"));
+        router.get('/login',(req,res)=>res.render("login"));
+        router.get('/logout',(req,res)=> res.send());
+
 
         return router;
     }
