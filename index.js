6b899006ee429d7507ce9d11e529a5c6baa740ce
Tommy Wong <xtommywongx@gmail.com>
2018-02-09 15:36:32 +0800

    basic setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-test
+// General Initialization
+require('dotenv').config();
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
+const REDIS_PORT = process.env.REDIS_PORT || 6379
+const NODE_ENV = process.env.NODE_ENV || 'development' 
+
+const knexFile = require('./knexfile')[NODE_ENV]
+const knex = require('knex')(knexFile)
+
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: REDIS_HOST,
+    port: REDIS_PORT
+})
+
+const ViewRouter = require('./ViewRouter');
+
+app.use('/', new ViewRouter().router());
